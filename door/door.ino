7829434b1c7467107280bbcdc7776358eb2d063f
rayos006 <tyler.ray@byu.edu>
2019-10-19 01:06:31 -0600

    Basic Publish code

--- door/door.ino
+++ door/door.ino
@@ -1,9 +1,63 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP8266WiFi.h> 
+#include <PubSubClient.h>
+#include "globals.h"
 
+//Wifi
+WiFiClient espClient;
+
+//MQTT
+PubSubClient client(espClient);
+
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+
+  //Define inputs and outputs
+
+  // WIFI Info and connect
+  WiFi.begin(ssid, wifi_password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.println("Waiting to connect…");
+  }
+
+  // IP Addresses
+  Serial.print("MY IP address: ");
+  Serial.println(WiFi.localIP());
+
+  //MQTT PUB/SUB
+  client.setServer(mqtt_hostname, 1883);
+}
+
+void connect_mqtt() {
+  // Loop until reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266 Client")) {
+      Serial.println("Connected");
+      } 
+    else {
+      Serial.println("failed, rc=");
+      Serial.print(client.state());
+      delay(5000);
+    }
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    connect_mqtt();
+  }
+  client.loop();
+  client.publish(periodic_topic, "closed");
+  delay(5000);
 
+
+
+
+
+
+  
 }
